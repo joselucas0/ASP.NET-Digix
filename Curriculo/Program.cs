5d72fe65eef9e7851f00dd3391545352d832398f
joselucas0 <jose.l@ufms.br>
2025-03-21 16:58:47 -0400

    feat: Adicionando Landing Page Teste

---
+++ Curriculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+namespace MeuPortfolio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            // Habilita arquivos estáticos e define o arquivo padrão como index.html
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
